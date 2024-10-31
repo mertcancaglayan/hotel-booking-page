a0a81fbb07533fbf80391b00e51afb83e532fc52
M.can <caglayan.mertcan37@gmail.com>
2024-10-31 17:08:39 +0300

    add components

---
+++ src/app/pages/booking-calender/booking-calender.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-calender',
+  standalone: true,
+  imports: [],
+  templateUrl: './booking-calender.component.html',
+  styleUrl: './booking-calender.component.scss'
+})
+export class BookingCalenderComponent {
+
+}
---
+++ src/app/pages/new-booking/new-booking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-booking',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-booking.component.html',
+  styleUrl: './new-booking.component.scss'
+})
+export class NewBookingComponent {
+
+}
